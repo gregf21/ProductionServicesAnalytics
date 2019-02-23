1520d39fc5ccdee3bfbb7093ecd01d46fadbb258
gregf21 <gregory12006@yahoo.com>
2019-02-23 14:29:05 -0500

    Add project files.

---
+++ ProductionServicesAnalyticsProgram/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace ProductionServicesAnalyticsProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.debugButton = new System.Windows.Forms.Button();
+            this.startDate = new System.Windows.Forms.DateTimePicker();
+            this.endDate = new System.Windows.Forms.DateTimePicker();
+            this.startDateLabel = new System.Windows.Forms.Label();
+            this.endDateLabel = new System.Windows.Forms.Label();
+            this.analysisTypeLabel = new System.Windows.Forms.Label();
+            this.analysisTypeListBox = new System.Windows.Forms.ListBox();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.analysisTypeCheckBoxList = new System.Windows.Forms.CheckedListBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(297, 296);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(82, 29);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // debugButton
+            // 
+            this.debugButton.Location = new System.Drawing.Point(22, 12);
+            this.debugButton.Name = "debugButton";
+            this.debugButton.Size = new System.Drawing.Size(75, 23);
+            this.debugButton.TabIndex = 1;
+            this.debugButton.Text = "Test";
+            this.debugButton.UseVisualStyleBackColor = true;
+            this.debugButton.Click += new System.EventHandler(this.debugButton_Click);
+            // 
+            // startDate
+            // 
+            this.startDate.Location = new System.Drawing.Point(22, 98);
+            this.startDate.Name = "startDate";
+            this.startDate.Size = new System.Drawing.Size(307, 26);
+            this.startDate.TabIndex = 2;
+            // 
+            // endDate
+            // 
+            this.endDate.Location = new System.Drawing.Point(22, 178);
+            this.endDate.Name = "endDate";
+            this.endDate.Size = new System.Drawing.Size(307, 26);
+            this.endDate.TabIndex = 3;
+            // 
+            // startDateLabel
+            // 
+            this.startDateLabel.AutoSize = true;
+            this.startDateLabel.Location = new System.Drawing.Point(18, 75);
+            this.startDateLabel.Name = "startDateLabel";
+            this.startDateLabel.Size = new System.Drawing.Size(87, 20);
+            this.startDateLabel.TabIndex = 4;
+            this.startDateLabel.Text = "Start Date:";
+            // 
+            // endDateLabel
+            // 
+            this.endDateLabel.AutoSize = true;
+            this.endDateLabel.Location = new System.Drawing.Point(22, 155);
+            this.endDateLabel.Name = "endDateLabel";
+            this.endDateLabel.Size = new System.Drawing.Size(77, 20);
+            this.endDateLabel.TabIndex = 5;
+            this.endDateLabel.Text = "End Date";
+            // 
+            // analysisTypeLabel
+            // 
+            this.analysisTypeLabel.AutoSize = true;
+            this.analysisTypeLabel.Location = new System.Drawing.Point(482, 38);
+            this.analysisTypeLabel.Name = "analysisTypeLabel";
+            this.analysisTypeLabel.Size = new System.Drawing.Size(109, 20);
+            this.analysisTypeLabel.TabIndex = 9;
+            this.analysisTypeLabel.Text = "Analysis Type:";
+            // 
+            // analysisTypeListBox
+            // 
+            this.analysisTypeListBox.FormattingEnabled = true;
+            this.analysisTypeListBox.ItemHeight = 20;
+            this.analysisTypeListBox.Location = new System.Drawing.Point(47, 388);
+            this.analysisTypeListBox.Name = "analysisTypeListBox";
+            this.analysisTypeListBox.Size = new System.Drawing.Size(120, 84);
+            this.analysisTypeListBox.TabIndex = 10;
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.ItemHeight = 20;
+            this.nameListBox.Location = new System.Drawing.Point(47, 546);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(120, 84);
+            this.nameListBox.TabIndex = 11;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(417, 330);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 12;
+            this.chart1.Text = "chart1";
+            // 
+            // analysisTypeCheckBoxList
+            // 
+            this.analysisTypeCheckBoxList.FormattingEnabled = true;
+            this.analysisTypeCheckBoxList.Items.AddRange(new object[] {
+            "Day",
+            "Month",
+            "Year"});
+            this.analysisTypeCheckBoxList.Location = new System.Drawing.Point(486, 116);
+            this.analysisTypeCheckBoxList.Name = "analysisTypeCheckBoxList";
+            this.analysisTypeCheckBoxList.Size = new System.Drawing.Size(175, 67);
+            this.analysisTypeCheckBoxList.TabIndex = 13;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(297, 644);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(83, 30);
+            this.updateButton.TabIndex = 14;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 712);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.analysisTypeCheckBoxList);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.analysisTypeListBox);
+            this.Controls.Add(this.analysisTypeLabel);
+            this.Controls.Add(this.endDateLabel);
+            this.Controls.Add(this.startDateLabel);
+            this.Controls.Add(this.endDate);
+            this.Controls.Add(this.startDate);
+            this.Controls.Add(this.debugButton);
+            this.Controls.Add(this.submitButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button debugButton;
+        private System.Windows.Forms.DateTimePicker startDate;
+        private System.Windows.Forms.DateTimePicker endDate;
+        private System.Windows.Forms.Label startDateLabel;
+        private System.Windows.Forms.Label endDateLabel;
+        private System.Windows.Forms.Label analysisTypeLabel;
+        private System.Windows.Forms.ListBox analysisTypeListBox;
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckedListBox analysisTypeCheckBoxList;
+        private System.Windows.Forms.Button updateButton;
+    }
+}
+
---
+++ ProductionServicesAnalyticsProgram/Form1.cs
@@ -0,0 +1,280 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductionServicesAnalyticsProgram
+{
+    public partial class Form1 : Form
+    {
+        //replace with time selection
+        int period = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+        //end replacement section
+
+        //key is index and value is full name separated by a space
+        Dictionary<String, int> indexByName;
+
+        int[] minutesPerWorkerByDay, minutesPerWorkerByMonth, minutesPerWorkerByYear;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            String user = "MBochar";
+            String pass = "g27cp1qm";
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://172.21.20.41/cepdotnet/CEPloginToCEP.aspx";
+
+            IWebElement username = driver.FindElement(By.Id("txtUserName"));
+            IWebElement password = driver.FindElement(By.Id("txtPassword"));
+
+            username.Clear();
+            username.SendKeys(user);
+
+            password.Clear();
+            password.SendKeys(pass);
+
+            IWebElement lgnBtn = driver.FindElement(By.Id("sbtLogin"));
+            lgnBtn.Click();
+
+
+            IReadOnlyCollection<IWebElement> elements;
+            indexByName = new Dictionary<String, int>();
+
+
+            //grab all names and store them in the dictionary
+            int indexForDictionary = 1;
+            indexByName.Add("All", 0);
+
+            driver.Url = "http://172.21.20.41/cepdotnet/CEPemployeeSearch.aspx";
+            elements = driver.FindElements(By.ClassName("CEPSub"));
+            String tempFirstName = "", tempLastName = "", tempElementText;
+
+            foreach (IWebElement element in elements)
+            {
+                if (element.Text.Contains("NoEmail@.xxx"))
+                    break;
+                tempLastName = element.Text.Substring(0, element.Text.IndexOf(",") - 1);
+                tempElementText = element.Text.Substring(tempLastName.Length + 3);
+                tempFirstName = tempElementText.Substring(0, tempElementText.IndexOf(" "));
+                indexByName.Add(tempFirstName + " " + tempLastName, indexForDictionary);
+                indexForDictionary++;
+            }
+
+
+
+            //make the array of minutes per worker
+
+            int dayIndexChange = 1, monthIndexChange = 1, yearIndexChange = 1;
+
+            if (analysisTypeCheckBoxList.GetItemChecked(0))
+            {
+                minutesPerWorkerByDay = new int[indexForDictionary * ((int)(endDate.Value - startDate.Value).TotalDays + 1)];
+            }
+            if (analysisTypeCheckBoxList.GetItemChecked(1))
+            {
+                minutesPerWorkerByMonth = new int[indexForDictionary * ((endDate.Value.Year - startDate.Value.Year) * 12 + (endDate.Value.Month - startDate.Value.Month))];
+            }
+            if (analysisTypeCheckBoxList.GetItemChecked(2))
+            {
+                minutesPerWorkerByYear = new int[indexForDictionary * ((endDate.Value.Year - startDate.Value.Year) * 12)];
+            }
+            //start analysis by day
+
+            String[] tempElementArray, tempWorkerDataArray;
+            String tempStartTime, tempEndTime;
+
+            int tempTime;
+
+
+            int cMonth = startDate.Value.Month, cDay = startDate.Value.Day, cYear = startDate.Value.Year;
+            for (int dateCounter = (int)(endDate.Value - startDate.Value).TotalDays + 1; dateCounter > 0; dateCounter--)
+            {
+                driver.Url = "http://172.21.20.41/cepdotnet/CEPHome.aspx?day=" + cDay + "&month=" + cMonth + "&year=" + cYear;
+
+                //grabs all events for current selected day
+                elements = driver.FindElements(By.ClassName("CEPDaySub"));
+                foreach (IWebElement element in elements)
+                {
+                    //data on individual workers starting at index 3 (inclusive)
+                    tempElementArray = element.Text.Split('\n');
+                    for (int i = 3; i < tempElementArray.Length; i++)
+                    {
+                        //0: first name
+                        //1: last name
+                        //2: start time
+                        //3: start time AM/PM
+                        //4: end time
+                        //5: end time AM/PM
+                        tempWorkerDataArray = tempElementArray[i].Split(' ');
+                        tempStartTime = tempWorkerDataArray[3];
+                        if (tempStartTime.Contains("MIDNIGHT") || tempStartTime.Contains("NOON"))
+                        {
+                            tempEndTime = tempWorkerDataArray[4];
+                            if (!(tempEndTime.Contains("MIDNIGHT") || tempEndTime.Contains("NOON")))
+                            {
+                                tempEndTime += " " + tempWorkerDataArray[5];
+                            }
+                        }
+                        else
+                        {
+                            tempStartTime += " " + tempWorkerDataArray[4];
+                            tempEndTime = tempWorkerDataArray[5];
+                            if (!(tempEndTime.Contains("MIDNIGHT") || tempEndTime.Contains("NOON")))
+                            {
+                                tempEndTime += " " + tempWorkerDataArray[6];
+                            }
+                        }
+
+                        tempTime = findMinutes(tempStartTime, tempEndTime);
+
+                        if (analysisTypeCheckBoxList.GetItemChecked(0))
+                        {
+                            minutesPerWorkerByDay[indexByName[tempWorkerDataArray[1] + " " + tempWorkerDataArray[2]] * dayIndexChange] += tempTime;
+                            minutesPerWorkerByDay[(dayIndexChange - 1) * indexByName.Count] = tempTime;
+                        }
+                        if (analysisTypeCheckBoxList.GetItemChecked(1))
+                        {
+                            minutesPerWorkerByMonth[indexByName[tempWorkerDataArray[1] + " " + tempWorkerDataArray[2]] * monthIndexChange] += tempTime;
+                            minutesPerWorkerByMonth[(monthIndexChange - 1) * indexByName.Count] = tempTime;
+                        }
+                        if (analysisTypeCheckBoxList.GetItemChecked(2))
+                        {
+                            minutesPerWorkerByYear[indexByName[tempWorkerDataArray[1] + " " + tempWorkerDataArray[2]] * yearIndexChange] += tempTime;
+                            minutesPerWorkerByYear[(yearIndexChange - 1) * indexByName.Count] = tempTime;
+                        }
+                    }
+
+                    cDay++;
+                    dayIndexChange++;
+                    int daysInMonth = System.DateTime.DaysInMonth(cYear, cMonth);
+                    if (cDay > daysInMonth)
+                    {
+                        cDay = 1;
+                        cMonth++;
+                        monthIndexChange++;
+                        if (cMonth > 12)
+                        {
+                            cMonth = 1;
+                            cYear++;
+                            yearIndexChange++;
+                        }
+
+                    }
+
+                }
+                
+            }
+            driver.Quit();
+
+
+            //sets up list boxes to current instance
+            analysisTypeListBox.DataSource = analysisTypeCheckBoxList.CheckedItems;
+            nameListBox.DataSource = indexByName.Keys.ToList();
+
+
+            //do grab index by name
+            //indexByName.FirstOrDefault(x => x.Value == i).Key + " - " + i + " : " + minutesPerWorker[i]
+            
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public int findMinutes(String startTime, String endTime)
+        {
+            int startHours = 0, startMinutes = 0, endHours = 0, endMinutes = 0;
+
+            if (startTime.Contains("NOON"))
+            {
+                startHours = 12;
+                startMinutes = 0;
+            }
+            else if(startTime.Contains("MIDNIGHT"))
+            {
+                startHours = 24;
+                startMinutes = 0;
+            }
+            else
+            {
+                startHours = Convert.ToInt32(startTime.Substring(0, startTime.IndexOf(':')));
+                startMinutes = Convert.ToInt32(startTime.Substring(startTime.IndexOf(':') + 1, 2));
+                if (startTime.Contains("PM"))
+                    startHours += 12;
+            }
+
+            if (endTime.Contains("NOON"))
+            {
+                endHours = 12;
+                endMinutes = 0;
+            }
+            else if (endTime.Contains("MIDNIGHT"))
+            {
+                endHours = 0;
+                endMinutes = 0;
+            }
+            else
+            {
+
+                
+
+                endHours = Convert.ToInt32(endTime.Substring(0, endTime.IndexOf(':')));
+                endMinutes = Convert.ToInt32(endTime.Substring(endTime.IndexOf(':') + 1, 2));
+                if (endTime.Contains("PM"))
+                    endHours += 12;
+            }
+
+            startMinutes += 60 * startHours;
+            endMinutes += 60 * endHours;
+
+            //flag for PM to AM switch
+            
+            //check if there is an PM to AM switch or full day
+            if(endMinutes - startMinutes <= 0)
+            {
+                endMinutes += 1440;
+            }
+            //returns for calulated amount of minutes
+            return endMinutes - startMinutes; 
+        }
+
+        //for testing methods and functionality of specific lines
+        private void debugButton_Click(object sender, EventArgs e)
+        {
+            //test the data for find minutes function
+
+            //15 hours, 900 minutes
+            Console.WriteLine(findMinutes("NOON", "3:00 AM"));
+
+            //12 hours, 720 minutes
+            Console.WriteLine(findMinutes("NOON", "MIDNIGHT"));
+
+            //12 hours, 720 minutes
+            Console.WriteLine(findMinutes("MIDNIGHT", "NOON"));
+
+            //3.5 hours, 210 minutes
+            Console.WriteLine(findMinutes("3:00 PM", "6:30 PM"));
+
+
+        }
+    }
+}
