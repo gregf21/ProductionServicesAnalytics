b4310dcf62da302d6b4e232dc00c048031e106db
gregf21 <gregory12006@yahoo.com>
2019-04-26 10:54:17 -0400

    Fixed window size.

--- ProductionServicesAnalyticsProgram/Form1.Designer.cs
+++ ProductionServicesAnalyticsProgram/Form1.Designer.cs
@@ -69,9 +69,10 @@
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(329, 82);
+            this.submitButton.Location = new System.Drawing.Point(219, 53);
+            this.submitButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.submitButton.Name = "submitButton";
-            this.submitButton.Size = new System.Drawing.Size(175, 91);
+            this.submitButton.Size = new System.Drawing.Size(117, 59);
             this.submitButton.TabIndex = 0;
             this.submitButton.Text = "Submit";
             this.submitButton.UseVisualStyleBackColor = true;
@@ -79,33 +80,37 @@
             // 
             // startDate
             // 
-            this.startDate.Location = new System.Drawing.Point(16, 95);
+            this.startDate.Location = new System.Drawing.Point(11, 62);
+            this.startDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.startDate.Name = "startDate";
-            this.startDate.Size = new System.Drawing.Size(307, 26);
+            this.startDate.Size = new System.Drawing.Size(206, 20);
             this.startDate.TabIndex = 2;
             // 
             // endDate
             // 
-            this.endDate.Location = new System.Drawing.Point(16, 147);
+            this.endDate.Location = new System.Drawing.Point(11, 96);
+            this.endDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.endDate.Name = "endDate";
-            this.endDate.Size = new System.Drawing.Size(307, 26);
+            this.endDate.Size = new System.Drawing.Size(206, 20);
             this.endDate.TabIndex = 3;
             // 
             // startDateLabel
             // 
             this.startDateLabel.AutoSize = true;
-            this.startDateLabel.Location = new System.Drawing.Point(12, 59);
+            this.startDateLabel.Location = new System.Drawing.Point(8, 38);
+            this.startDateLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.startDateLabel.Name = "startDateLabel";
-            this.startDateLabel.Size = new System.Drawing.Size(87, 20);
+            this.startDateLabel.Size = new System.Drawing.Size(58, 13);
             this.startDateLabel.TabIndex = 4;
             this.startDateLabel.Text = "Start Date:";
             // 
             // endDateLabel
             // 
             this.endDateLabel.AutoSize = true;
-            this.endDateLabel.Location = new System.Drawing.Point(16, 124);
+            this.endDateLabel.Location = new System.Drawing.Point(11, 81);
+            this.endDateLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.endDateLabel.Name = "endDateLabel";
-            this.endDateLabel.Size = new System.Drawing.Size(77, 20);
+            this.endDateLabel.Size = new System.Drawing.Size(52, 13);
             this.endDateLabel.TabIndex = 5;
             this.endDateLabel.Text = "End Date";
             // 
@@ -113,28 +118,29 @@
             // 
             this.analysisTypeLabel.AutoSize = true;
             this.analysisTypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.analysisTypeLabel.Location = new System.Drawing.Point(57, 188);
+            this.analysisTypeLabel.Location = new System.Drawing.Point(38, 122);
+            this.analysisTypeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.analysisTypeLabel.Name = "analysisTypeLabel";
-            this.analysisTypeLabel.Size = new System.Drawing.Size(168, 29);
+            this.analysisTypeLabel.Size = new System.Drawing.Size(109, 20);
             this.analysisTypeLabel.TabIndex = 9;
             this.analysisTypeLabel.Text = "Analysis Type:";
             // 
             // analysisTypeListBox
             // 
             this.analysisTypeListBox.FormattingEnabled = true;
-            this.analysisTypeListBox.ItemHeight = 20;
-            this.analysisTypeListBox.Location = new System.Drawing.Point(20, 313);
+            this.analysisTypeListBox.Location = new System.Drawing.Point(13, 203);
+            this.analysisTypeListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.analysisTypeListBox.Name = "analysisTypeListBox";
-            this.analysisTypeListBox.Size = new System.Drawing.Size(244, 84);
+            this.analysisTypeListBox.Size = new System.Drawing.Size(164, 56);
             this.analysisTypeListBox.TabIndex = 10;
             // 
             // nameListBox
             // 
             this.nameListBox.FormattingEnabled = true;
-            this.nameListBox.ItemHeight = 20;
-            this.nameListBox.Location = new System.Drawing.Point(20, 403);
+            this.nameListBox.Location = new System.Drawing.Point(13, 262);
+            this.nameListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.nameListBox.Name = "nameListBox";
-            this.nameListBox.Size = new System.Drawing.Size(244, 184);
+            this.nameListBox.Size = new System.Drawing.Size(164, 121);
             this.nameListBox.TabIndex = 11;
             // 
             // chart1
@@ -143,13 +149,14 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(270, 188);
+            this.chart1.Location = new System.Drawing.Point(180, 122);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(806, 399);
+            this.chart1.Size = new System.Drawing.Size(537, 259);
             this.chart1.TabIndex = 12;
             this.chart1.Text = "chart1";
             // 
@@ -161,17 +168,19 @@
             "Day",
             "Month",
             "Year"});
-            this.analysisTypeCheckBoxList.Location = new System.Drawing.Point(20, 220);
+            this.analysisTypeCheckBoxList.Location = new System.Drawing.Point(13, 143);
+            this.analysisTypeCheckBoxList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.analysisTypeCheckBoxList.Name = "analysisTypeCheckBoxList";
-            this.analysisTypeCheckBoxList.Size = new System.Drawing.Size(244, 88);
+            this.analysisTypeCheckBoxList.Size = new System.Drawing.Size(164, 49);
             this.analysisTypeCheckBoxList.TabIndex = 13;
             this.analysisTypeCheckBoxList.SelectedIndexChanged += new System.EventHandler(this.analysisTypeCheckBoxList_SelectedIndexChanged);
             // 
             // updateButton
             // 
-            this.updateButton.Location = new System.Drawing.Point(510, 82);
+            this.updateButton.Location = new System.Drawing.Point(340, 53);
+            this.updateButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.updateButton.Name = "updateButton";
-            this.updateButton.Size = new System.Drawing.Size(175, 91);
+            this.updateButton.Size = new System.Drawing.Size(117, 59);
             this.updateButton.TabIndex = 14;
             this.updateButton.Text = "Update Chart";
             this.updateButton.UseVisualStyleBackColor = true;
@@ -182,9 +191,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(208, 9);
+            this.label1.Location = new System.Drawing.Point(139, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(677, 37);
+            this.label1.Size = new System.Drawing.Size(475, 26);
             this.label1.TabIndex = 15;
             this.label1.Text = "Virginia Tech Production Services Analytics";
             // 
@@ -192,50 +202,56 @@
             // 
             this.totalHoursLabel.AutoSize = true;
             this.totalHoursLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.totalHoursLabel.Location = new System.Drawing.Point(418, 603);
+            this.totalHoursLabel.Location = new System.Drawing.Point(279, 392);
+            this.totalHoursLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.totalHoursLabel.Name = "totalHoursLabel";
-            this.totalHoursLabel.Size = new System.Drawing.Size(215, 37);
+            this.totalHoursLabel.Size = new System.Drawing.Size(148, 26);
             this.totalHoursLabel.TabIndex = 16;
             this.totalHoursLabel.Text = "Total Hours: ";
             // 
             // usernameBox
             // 
-            this.usernameBox.Location = new System.Drawing.Point(805, 102);
+            this.usernameBox.Location = new System.Drawing.Point(537, 66);
+            this.usernameBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.usernameBox.Name = "usernameBox";
-            this.usernameBox.Size = new System.Drawing.Size(233, 26);
+            this.usernameBox.Size = new System.Drawing.Size(157, 20);
             this.usernameBox.TabIndex = 17;
             // 
             // passwordBox
             // 
-            this.passwordBox.Location = new System.Drawing.Point(805, 134);
+            this.passwordBox.Location = new System.Drawing.Point(537, 87);
+            this.passwordBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.passwordBox.Name = "passwordBox";
-            this.passwordBox.Size = new System.Drawing.Size(233, 26);
+            this.passwordBox.Size = new System.Drawing.Size(157, 20);
             this.passwordBox.TabIndex = 18;
             this.passwordBox.UseSystemPasswordChar = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(716, 105);
+            this.label2.Location = new System.Drawing.Point(477, 68);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 19;
             this.label2.Text = "Username";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(721, 137);
+            this.label3.Location = new System.Drawing.Point(481, 89);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 20;
             this.label3.Text = "Password";
             // 
             // excelExport
             // 
-            this.excelExport.Location = new System.Drawing.Point(20, 593);
+            this.excelExport.Location = new System.Drawing.Point(13, 385);
+            this.excelExport.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.excelExport.Name = "excelExport";
-            this.excelExport.Size = new System.Drawing.Size(244, 53);
+            this.excelExport.Size = new System.Drawing.Size(163, 34);
             this.excelExport.TabIndex = 21;
             this.excelExport.Text = "Export to Excel";
             this.excelExport.UseVisualStyleBackColor = true;
@@ -243,9 +259,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(270, 593);
+            this.button1.Location = new System.Drawing.Point(180, 385);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 53);
+            this.button1.Size = new System.Drawing.Size(95, 34);
             this.button1.TabIndex = 22;
             this.button1.Text = "Help";
             this.button1.UseVisualStyleBackColor = true;
@@ -254,18 +271,19 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 649);
+            this.label4.Location = new System.Drawing.Point(11, 422);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.Size = new System.Drawing.Size(69, 13);
             this.label4.TabIndex = 23;
             this.label4.Text = "Version 1.0.0";
             // 
             // overtimeCheck
             // 
-            this.overtimeCheck.Location = new System.Drawing.Point(1080, 522);
-            this.overtimeCheck.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.overtimeCheck.Location = new System.Drawing.Point(720, 339);
+            this.overtimeCheck.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.overtimeCheck.Name = "overtimeCheck";
-            this.overtimeCheck.Size = new System.Drawing.Size(192, 65);
+            this.overtimeCheck.Size = new System.Drawing.Size(128, 42);
             this.overtimeCheck.TabIndex = 25;
             this.overtimeCheck.Text = "Overtime Check";
             this.overtimeCheck.UseVisualStyleBackColor = true;
@@ -273,10 +291,10 @@
             // 
             // hoursCheck
             // 
-            this.hoursCheck.Location = new System.Drawing.Point(1286, 522);
-            this.hoursCheck.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.hoursCheck.Location = new System.Drawing.Point(857, 339);
+            this.hoursCheck.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.hoursCheck.Name = "hoursCheck";
-            this.hoursCheck.Size = new System.Drawing.Size(192, 65);
+            this.hoursCheck.Size = new System.Drawing.Size(128, 42);
             this.hoursCheck.TabIndex = 26;
             this.hoursCheck.Text = "Hours Check";
             this.hoursCheck.UseVisualStyleBackColor = true;
@@ -290,10 +308,10 @@
             listViewItem1,
             listViewItem2,
             listViewItem3});
-            this.checkerListView.Location = new System.Drawing.Point(1080, 188);
-            this.checkerListView.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.checkerListView.Location = new System.Drawing.Point(720, 122);
+            this.checkerListView.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.checkerListView.Name = "checkerListView";
-            this.checkerListView.Size = new System.Drawing.Size(398, 330);
+            this.checkerListView.Size = new System.Drawing.Size(267, 216);
             this.checkerListView.TabIndex = 27;
             this.checkerListView.UseCompatibleStateImageBehavior = false;
             // 
@@ -303,19 +321,19 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(1080, 152);
-            this.progressBar1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.progressBar1.Location = new System.Drawing.Point(720, 99);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(398, 32);
+            this.progressBar1.Size = new System.Drawing.Size(265, 21);
             this.progressBar1.TabIndex = 28;
             // 
             // percentageLabel
             // 
             this.percentageLabel.AutoSize = true;
-            this.percentageLabel.Location = new System.Drawing.Point(1076, 95);
+            this.percentageLabel.Location = new System.Drawing.Point(717, 62);
             this.percentageLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.percentageLabel.Name = "percentageLabel";
-            this.percentageLabel.Size = new System.Drawing.Size(168, 20);
+            this.percentageLabel.Size = new System.Drawing.Size(111, 13);
             this.percentageLabel.TabIndex = 29;
             this.percentageLabel.Text = "Data Grab Completion";
             // 
@@ -327,18 +345,19 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(800, 70);
+            this.label5.Location = new System.Drawing.Point(533, 45);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(187, 29);
+            this.label5.Size = new System.Drawing.Size(126, 20);
             this.label5.TabIndex = 30;
             this.label5.Text = "CEP Information";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.ClientSize = new System.Drawing.Size(1721, 1050);
+            this.ClientSize = new System.Drawing.Size(1014, 443);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.percentageLabel);
             this.Controls.Add(this.progressBar1);
@@ -366,9 +385,9 @@
             this.Controls.Add(this.startDate);
             this.Controls.Add(this.submitButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Production Services Analytics";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
