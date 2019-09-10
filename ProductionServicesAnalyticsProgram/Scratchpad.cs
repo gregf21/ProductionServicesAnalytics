844031b67c19d4a0b9614d937d62916fe32f5742
Matthew Bocharnikov <mattb99@vt.edu>
2019-09-10 00:40:04 -0400

    Added some functionality to be implemented in Scratchpad

--- ProductionServicesAnalyticsProgram/Scratchpad.cs
+++ ProductionServicesAnalyticsProgram/Scratchpad.cs
@@ -13,9 +13,15 @@ namespace ProductionServicesAnalyticsProgram
 {
     public partial class Scratchpad : Form
     {
+        //variables for posted shifts
         DateTime[] dates;
         Worker[] workers;
         WorkingTime[,] workingTimes;
+
+        Shift[] openShifts;
+
+        DateTime currentDate;
+
 
         public Scratchpad()
         {
@@ -37,6 +43,67 @@ namespace ProductionServicesAnalyticsProgram
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+        
+        public void retrieveOpenShiftData()
+        {
+            //access shift data and possible worker information
+            //for now there is temporary data for 3 shifts below
+
+            Shift shift1 = new Shift("Culture Show", new DateTime(2019, 9, 9, 8, 0, 0),
+                new DateTime(2019, 9, 9, 23, 0, 0), 4, 0);
+            Shift shift2 = new Shift("CAV Shift", new DateTime(2019, 9, 10, 14, 0, 0),
+                new DateTime(2019, 9, 10, 16, 0, 0), 1, 0);
+            Shift shift3 = new Shift("Musical", new DateTime(2019, 9, 11, 7, 0, 0),
+                new DateTime(2019, 9, 11, 23, 0, 0), 1, 0);
+        }
+
+        private void updatePossibleWorkers()
+        {
+            int openShiftLastIndex = 0;
+            Worker[] tempWorkers;
+            for (int openShiftIndex = 0; openShiftIndex < openShifts.Length; openShiftIndex++)
+            {
+
+                for (int i = 0; i < openShifts[openShiftIndex].getShiftPossibleWorkers().Length; i++)
+                {
+                    tempWorkers = openShifts[openShiftIndex].getShiftPossibleWorkers();
+                    //if (//overtime or dual booking)
+                }
+            }
+        }
+
+        private bool checkWorkerForOvertime(Worker worker)
+        {
+
+            return true;
+        }
+
+        private bool checkWorkerForOverlap(Worker worker)
+        {
+            
+            return true;
+        }
+
+        //returns the index from dates that matches the parameter date
+        private int getIndexFromDate(DateTime date)
+        {
+            int counter = -1;
+            for(int i = 0; i < dates.Length; i++)
+            {
+                if (!checkForEqualDates(date, dates[i]))
+                    counter++;
+                else
+                    break;
+            }
+            return counter;
+        }
+
+        private bool checkForEqualDates(DateTime d1, DateTime d2)
+        {
+            if (d1.Year == d2.Year && d1.Month == d2.Month && d1.Day == d2.Day)
+                return true;
+            return false;
         }
     }
 }
---
+++ ProductionServicesAnalyticsProgram/Shift.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionServicesAnalyticsProgram
+{
+    public class Shift
+    {
+        private String name;
+        private DateTime startTime, endTime;
+        private int totalPositions, filledPositions;
+
+        //these three arrays work off of the same index
+        private Worker[] possibleWorkers;
+        private DateTime[] possibleWorkerStartTimes, possibleWorkerEndTimes;
+        //1 = chosen and 0 = not chosen and -1 is not available 
+        private int[] chosenWorkers;
+
+        public Shift(String n, DateTime sT, DateTime eT, int tP, int fP)
+        {
+            name = n;
+            startTime = sT;
+            endTime = eT;
+            totalPositions = tP;
+            filledPositions = fP;
+        }
+
+        public String getShiftName()
+        {
+            return name;
+        }
+        public DateTime getShiftStartTime()
+        {
+            return startTime;
+        }
+
+        public DateTime getShiftEndTime()
+        {
+            return endTime;
+        }
+
+        public int getShiftTotalPositions()
+        {
+            return totalPositions;
+        }
+
+        public int getShiftFilledPositions()
+        {
+            return filledPositions;
+        }
+
+        public void setShiftFilledPositions(int fP)
+        {
+            filledPositions = fP;
+        }
+
+        public Worker[] getShiftPossibleWorkers()
+        {
+            return possibleWorkers;
+        }
+
+        public DateTime[] getShiftPossibleWorkerStartTimes()
+        {
+            return possibleWorkerStartTimes;
+        }
+
+        public DateTime[] getShiftPossibleWorkerEndTimes()
+        {
+            return possibleWorkerEndTimes;
+        }
+
+        public void setPossibleWorkers(Worker[] pW)
+        {
+            possibleWorkers = pW;
+        }
+
+        public void setPossibleWorkerStartTimes(DateTime[] pWST)
+        {
+            possibleWorkerStartTimes = pWST;
+        }
+
+        public void setPossibleWorkerEndTimes(DateTime[] pWET)
+        {
+            possibleWorkerEndTimes = pWET;
+        }
+
+        public int[] getChosenWorkers()
+        {
+            return chosenWorkers;
+        }
+
+        public void setChosenWorkers(int[] cW)
+        {
+            chosenWorkers = cW;
+        }
+
+        public void setChosenWorkersAtIndex(int i, int value)
+        {
+            chosenWorkers[i] = value;
+        }
+    }
+}
