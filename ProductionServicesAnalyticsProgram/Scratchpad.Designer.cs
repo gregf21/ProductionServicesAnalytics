c9e549bbceec55f9cf0b41cf3c2ac56a83f7882d
gregf21 <gregory12006@yahoo.com>
2019-09-09 22:28:00 -0400

    Added submit button

--- ProductionServicesAnalyticsProgram/Scratchpad.Designer.cs
+++ ProductionServicesAnalyticsProgram/Scratchpad.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1067, 519);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Scratchpad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(979, 589);
+            this.ClientSize = new System.Drawing.Size(1192, 595);
+            this.Controls.Add(this.button1);
             this.Name = "Scratchpad";
             this.Text = "Scratchpad";
             this.Load += new System.EventHandler(this.Scratchpad_Load);
@@ -43,5 +55,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- ProductionServicesAnalyticsProgram/Scratchpad.cs
+++ ProductionServicesAnalyticsProgram/Scratchpad.cs
@@ -21,5 +21,10 @@ namespace ProductionServicesAnalyticsProgram
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
