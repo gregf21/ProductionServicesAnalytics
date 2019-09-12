6cbe24e41d419077bc73905c33cef60c1d10bb81
gregf21 <gregory12006@yahoo.com>
2019-09-12 15:27:06 -0400

    Updated UI. See comments.

--- ProductionServicesAnalyticsProgram/Scratchpad.Designer.cs
+++ ProductionServicesAnalyticsProgram/Scratchpad.Designer.cs
@@ -28,34 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.submit = new System.Windows.Forms.Button();
+            this.events = new System.Windows.Forms.ListView();
+            this.addToShift = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.students = new System.Windows.Forms.ListView();
+            this.editedShifts = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // submit
             // 
-            this.button1.Location = new System.Drawing.Point(1067, 519);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 64);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.submit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.Location = new System.Drawing.Point(758, 519);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(568, 64);
+            this.submit.TabIndex = 0;
+            this.submit.Text = "Submit Changes";
+            this.submit.UseVisualStyleBackColor = false;
+            this.submit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // events
+            // 
+            this.events.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.events.HideSelection = false;
+            this.events.Location = new System.Drawing.Point(758, 75);
+            this.events.Name = "events";
+            this.events.Size = new System.Drawing.Size(568, 438);
+            this.events.TabIndex = 2;
+            this.events.UseCompatibleStateImageBehavior = false;
+            this.events.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // addToShift
+            // 
+            this.addToShift.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.addToShift.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addToShift.Location = new System.Drawing.Point(409, 166);
+            this.addToShift.Name = "addToShift";
+            this.addToShift.Size = new System.Drawing.Size(343, 108);
+            this.addToShift.TabIndex = 3;
+            this.addToShift.Text = "Add Students to Selected Shift ->";
+            this.addToShift.UseVisualStyleBackColor = false;
+            this.addToShift.Click += new System.EventHandler(this.addToShift_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1024, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Shifts";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(281, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Available Students";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(507, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 120);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Display Selected Shift Data, TEST USE ONLY";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // students
+            // 
+            this.students.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.students.Location = new System.Drawing.Point(13, 75);
+            this.students.Name = "students";
+            this.students.Size = new System.Drawing.Size(390, 508);
+            this.students.TabIndex = 7;
+            this.students.UseCompatibleStateImageBehavior = false;
+            // 
+            // editedShifts
+            // 
+            this.editedShifts.Cursor = System.Windows.Forms.Cursors.No;
+            this.editedShifts.FormattingEnabled = true;
+            this.editedShifts.ItemHeight = 20;
+            this.editedShifts.Location = new System.Drawing.Point(409, 339);
+            this.editedShifts.Name = "editedShifts";
+            this.editedShifts.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.editedShifts.Size = new System.Drawing.Size(343, 244);
+            this.editedShifts.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(459, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(267, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Assigned, Unsubmitted Shifts";
             // 
             // Scratchpad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1192, 595);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1338, 595);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.editedShifts);
+            this.Controls.Add(this.students);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addToShift);
+            this.Controls.Add(this.events);
+            this.Controls.Add(this.submit);
             this.Name = "Scratchpad";
             this.Text = "Scratchpad";
             this.Load += new System.EventHandler(this.Scratchpad_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.ListView events;
+        private System.Windows.Forms.Button addToShift;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView students;
+        private System.Windows.Forms.ListBox editedShifts;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolTip toolTip;
     }
 }
--- ProductionServicesAnalyticsProgram/Scratchpad.cs
+++ ProductionServicesAnalyticsProgram/Scratchpad.cs
@@ -8,6 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+
+    Select shift to book people for. Once selected, update available student list to signed up students.
+    Check students to add to shift and click "add students to selected shift."
+    Complete for as many shifts as necessary.
+    Once shifts are booked, press submit to send all edited shifts to CEP.
+
+*/
+
 
 namespace ProductionServicesAnalyticsProgram
 {
@@ -21,6 +30,9 @@ namespace ProductionServicesAnalyticsProgram
         Shift[] openShifts;
 
         DateTime currentDate;
+
+        string[] eventArray = new string[3];
+        string[] studentArray = new string[2];
 
 
         public Scratchpad()
@@ -37,12 +49,49 @@ namespace ProductionServicesAnalyticsProgram
         }
         private void Scratchpad_Load(object sender, EventArgs e)
         {
-        
+            events.View = View.Details;
+            events.GridLines = true;
+            events.FullRowSelect = true;
+            events.Columns.Add("Event Name", 125);
+            events.Columns.Add("Event Day", 100);
+            events.Columns.Add("Event Time", 100);
+            events.MultiSelect = false;
+            students.View = View.Details;
+            students.GridLines = true;
+            students.FullRowSelect = true;
+            students.Columns.Add("Student Name", 100);
+            students.Columns.Add("Requested Work Time", 125);
+            students.CheckBoxes = true;
+            toolTip.InitialDelay = 2000;
+            toolTip.SetToolTip(students, "Choose students for the selected shift.");
+            toolTip.SetToolTip(events, "Select an event to view signed up students.");
+            eventArray[0] = "EventTest1";
+            eventArray[1] = "EventDay1";
+            eventArray[2] = "EventTime1";
+            ListViewItem item;
+            item = new ListViewItem(eventArray);
+            events.Items.Add(item);
+            eventArray[0] = "EventTest2";
+            eventArray[1] = "EventDay2";
+            eventArray[2] = "EventTime2";
+            ListViewItem item2;
+            item2 = new ListViewItem(eventArray);
+            events.Items.Add(item2);
+            studentArray[0] = "StudentTest1";
+            studentArray[1] = "StudentTime1";
+            ListViewItem item3;
+            item3 = new ListViewItem(studentArray);
+            students.Items.Add(item3);
+            studentArray[0] = "StudentTest2";
+            studentArray[1] = "StudentTime2";
+            ListViewItem item4;
+            item4 = new ListViewItem(studentArray);
+            students.Items.Add(item4);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            editedShifts.Items.Clear();
         }
         
         public void retrieveOpenShiftData()
@@ -104,6 +153,21 @@ namespace ProductionServicesAnalyticsProgram
             if (d1.Year == d2.Year && d1.Month == d2.Month && d1.Day == d2.Day)
                 return true;
             return false;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(events.SelectedItems[0].SubItems[0].Text + " " + events.SelectedItems[0].SubItems[1].Text + " " + events.SelectedItems[0].SubItems[2].Text);
+        }
+
+        private void addToShift_Click(object sender, EventArgs e)
+        {
+            editedShifts.Items.Add(events.SelectedItems[0].SubItems[0].Text);
         }
     }
 }
